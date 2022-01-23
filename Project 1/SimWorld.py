3dd988b54f749a2bda1430781908c460d6123b10
Mathipe98 <56786723+Mathipe98@users.noreply.github.com>
2022-01-23 17:32:34 +0100

    Added tile encoding

--- Project 1/SimWorld.py
+++ Project 1/SimWorld.py
@@ -1,5 +1,6 @@
-from typing import Any, Dict
+from typing import Any, Dict, Union
 import numpy as np
+from tile_coding import create_tilings, get_tile_coding
 
 
 class Simworld:
@@ -9,17 +10,38 @@ class Simworld:
                  n_episodes: int,
                  n_steps: int,
                  funcapp: bool,
-                 network_dim: tuple,
-                 act_lr: float,
-                 act_decay: float,
-                 act_disc: float,
-                 crit_lr: float,
-                 crit_decay: float,
-                 crit_disc: float,
+                 network_dim: Union[tuple, None],
+                 act_crit_params: list[float],
+                 # act_lr: float,
+                 # act_decay: float,
+                 # act_disc: float,
+                 # crit_lr: float,
+                 # crit_decay: float,
+                 # crit_disc: float,
                  epsilon: float,
                  display: Any,  # <- wtf does this do?
-                 frame_delay: float) -> None:
-        pass
+                 frame_delay: float,
+                 **kwargs: Any) -> None:
+        assert 0 <= game <= 2, "Game number must be between 0-2"
+        assert n_episodes >= 1, "Number of episodes must be >= 1"
+        assert n_steps > 0, "Number of steps must be > 0"
+        if network_dim is not None:
+            for dim in network_dim:
+                assert dim > 0, "Network dimensions cannot be negative"
+        for param in act_crit_params:
+            assert 0.0 <= param <= 1, "Learning rate, decay, and discount parameters must be between 0-1"
+        assert 0.0 <= epsilon <= 1, "Epsilon must be in the range 0-1"
+        assert frame_delay >= 0.0, "Frame delay must be a positive value"
+        self.game = game
+        self.n_episodes = n_episodes
+        self.n_steps = n_steps
+        self.funcapp = funcapp
+        self.network_dim = network_dim
+        self.act_crit_params = act_crit_params
+        self.epsilon = epsilon
+        self.display = display
+        self.frame_delay = frame_delay
+        self.__dict__.update(kwargs)
 
         """
         Thoughts:
@@ -38,5 +60,32 @@ class Simworld:
         Maybe coarse coding for all games is possible, look into this.
         """
 
-    def generate_table(self, game: int) -> dict:
-        pass
+    def generate_states(self, game: int) -> np.ndarray:
+        if game == 0:
+            # Game is pole-balancing, we need a general way to represent the state
+            # State parameters: cart position, cart speed, pole angle, pole angular velocity (x, x', theta, theta')
+            # Thoughts: binning; for each variable, put the values into discrete bins (buckets) of values
+            # Include tau (timestep) in the state, but only 2 bins; 1 bin is when tau < 300 (which is the goal), and the other is tau >= 300. Then we can check whether tau is in the second bin when checking if winning state
+            # First create ranges for all variables such that we can check states easily in bins
+            x_range = [-2.4, 2.4] # Note: when the state for this is 0 or the length of the bin, then we lose. Same goes for angle
+            x_v_range = [-1, 1]
+            ang_range = [-0.21, 0.21]
+            ang_v_range = [-0.1, 0.1]
+            t_range = [0, 300]
+            ranges = [x_range, x_v_range, ang_range, ang_v_range, t_range]
+            # Setup bin (bucket) parameters
+            n_tilings = 3
+            x_bins = 4
+            x_v_bins = 4
+            ang_bins = 8
+            ang_v_bins = 8
+            t_bins = 2
+            # Create a nested list that uses the same bins for every tiling
+            bins = [x_bins, x_v_bins, ang_bins, ang_v_bins, t_bins]
+            bins = [bins[:] for _ in range(n_tilings)]
+            
+            pass
+        else:
+            pass
+    
+ 
---
+++ Project 1/tile_coding.py
@@ -0,0 +1,151 @@
+import numpy as np
+
+
+def create_single_tiling(
+        feat_range: list,
+        bins: int,
+        offset: float) -> np.ndarray:
+    """Create a single tiling that corresponds to the tile of a single feature
+
+    Args:
+        feat_range (np.ndarray): Array that contains ranges of values for the feature (low - high)
+        bins (int): The number of different bins/buckets to put the feature into
+        offset (float): Offset of the bin-values, i.e. we can "nudge" the bin in a certain direction
+
+    Returns:
+        np.ndarray: Array that contains the tiles for the feature, where the tile is evenly divided into bins
+    """
+
+    return np.linspace(feat_range[0], feat_range[1], bins+1)[1:-1] + offset
+
+
+def create_tilings(feature_ranges: list, number_tilings: int, bins: list, offsets: list) -> np.ndarray:
+    """Create an array of multiple tilings for every feature, with every tile having individual bins and offsets.
+    So you can send in say 2 features, with ranges = [[0, 1], [5, 10]], meaning feat1 has range 0-1, and feat2 5-10.
+    And say you have number_tilings = 2. Then we say that we want to have 2 tiles for 2 features. We can then send
+    in the following:
+        bins=[[10, 5], [4, 8]]
+        offsets=[[0.1, 0.3], [0.5, 0.1]]
+    What does this mean? Well the first tile will get the following configuration:
+        feat1 is divided into 10 bins, feat2 is divided into 5 bins
+        feat1's bins are offset by 0.1, feat2's bins are offset by 0.3
+    So every array contains the bins for the respective variables and the offsets of those bins for every feature
+    This will be used to represent the states for the RL agent
+
+    Args:
+        feature_ranges (list): A 2-dimensional array containing the ranges of every feature
+        number_tilings (int): An integer describing the number of tiles wanted
+        bins (list): A 2-dimensional array with n-dimensional internal arrays, where n is the number of features
+        offsets (list): A 2-dimensional array with n-dimensional internal arrays, where n is the number of features
+
+    Returns:
+        np.ndarray: A tile encoding of the features, given their ranges, bins, and offsets
+    """
+    tilings = []
+    # for each tiling
+    for tile_i in range(number_tilings):
+        tiling_bin = bins[tile_i]
+        tiling_offset = offsets[tile_i]
+
+        tiling = []
+        # for each feature dimension
+        for feat_i in range(len(feature_ranges)):
+            feat_range = feature_ranges[feat_i]
+            # tiling for 1 feature
+            feat_tiling = create_single_tiling(
+                feat_range, tiling_bin[feat_i], tiling_offset[feat_i])
+            tiling.append(feat_tiling)
+        tilings.append(tiling)
+    for coding in tilings:
+        if len(coding) > 1:
+            result = coding[1]
+            for i in range(1, len(coding)):
+                result = np.concatenate((result, coding[i]), axis=None)
+    return np.array(tilings, dtype='object')
+
+
+def get_tile_coding(feature_values: list, tilings: list) -> np.ndarray:
+    """The final function in the tile-coding hierarchy; this function takes in a set of features and its corresponding tiles,
+    and outputs the tile-encoding for that particular set of features.
+    In context of the RL agent, the tiles are the states, and the state-space consists of all possible configurations of the tiles.
+
+    Args:
+        feature_values (list): An array containing exact values for all the n features
+        tilings (list): An array representing the tilings for the features, with associated bins and offsets for each feature
+
+    Returns:
+        np.ndarray: An array containing the tile-encoding of the particular features
+    """
+    num_dims = len(feature_values)
+    feat_codings = []
+    for tiling in tilings:
+        feat_coding = []
+        for i in range(num_dims):
+            feat_i = feature_values[i]
+            tiling_i = tiling[i]  # tiling on that dimension
+            coding_i = np.digitize(feat_i, tiling_i)
+            feat_coding.append(coding_i)
+        feat_codings.append(feat_coding)
+    return np.array(feat_codings)
+
+def test_pole_tilings() -> None:
+    # Features in following order: cart position, cart velocity, angle position, angle velocity, timestep
+    cart_pos_range = [-2.4, 2.4]
+    cart_speed_range = [-2, 2]
+    angle_pos_range = [-0.21, 0.21]
+    angle_vel_range = [-2, 2]
+    timestep_range = [0, 300]
+    feature_ranges = [cart_pos_range, cart_speed_range, angle_pos_range, angle_vel_range, timestep_range]
+    n_tilings = 1
+    # Configure the number of bins and offsets for each feature
+    cart_pos_bins = 4
+    cart_pos_offset = 0
+    cart_speed_bins = 4
+    cart_speed_offset = 0
+    angle_pos_bins = 8
+    angle_pos_offset = 0
+    angle_vel_bins = 8
+    angle_vel_offset = 0
+    # Timestep gets 2 bins, because its only purpose is to determine whether or not we're in a winning state
+    timestep_bins = 2
+    # Offset = 150 => timestep <= 299 => we're not winning, timestep >= 300 => we are winning
+    # This because if tau <= 299, the tile encoding will result in 0 with this offset
+    # (Reason for 150: linspace takes range/2 = 150, so offset 150 => bin-value becomes 300)
+    timestep_offset = 150
+    feat_bins = [[cart_pos_bins, cart_speed_bins, angle_pos_bins, angle_vel_bins, timestep_bins]]
+    feat_offsets = [[cart_pos_offset, cart_speed_offset, angle_pos_offset, angle_vel_offset, timestep_offset]]
+    test_tilings = create_tilings(feature_ranges, n_tilings, feat_bins, feat_offsets)
+    print(test_tilings)    
+    test_encoding = [1.2, -100, -0.3, 2, 299]
+    test_result = get_tile_coding(test_encoding, test_tilings)
+    print(test_result)
+
+def test_other_thing() -> None:
+    feature_ranges = [[-1, 1], [2, 5], [0, 10], [-2.4, 2.4], [-1, 1]]
+    number_tilings = 2
+    bins = [[4, 4, 8, 8, 8], [5, 5, 9, 9, 9]]
+    offsets = [[0, 0, 0, 0, 0], [1, 1, 1, 1, 1]]
+
+    tilings = create_tilings(feature_ranges, number_tilings, bins, offsets)
+    print(tilings)
+    feature = [0.1, 2.5, 0.3, 0.2, 0]
+
+    coding = get_tile_coding(feature, tilings)
+    print(coding)
+
+if __name__ == '__main__':
+    """
+    feature_ranges = [[-1, 1], [2, 5]]  # 2 features
+    number_tilings = 1
+    bins = [[10, 10, 10], [10, 10], [10, 10]]  # each tiling has a 10*10 grid
+    offsets = [[0, 0, 0], [0.2, 1], [0.4, 1.5]]
+
+    tilings = create_tilings(feature_ranges, number_tilings, bins, offsets)
+
+    feature = [0.1, 2.5, 0.3]
+
+    coding = get_tile_coding(feature, tilings)
+    print(coding)
+    """
+    test_pole_tilings()
+    test_other_thing()
